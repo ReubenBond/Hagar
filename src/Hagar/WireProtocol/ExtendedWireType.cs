9a79d3259abfe73fb0893aa437dfa8d75b9e551f
ReubenBond <a-rebond@microsoft.com>
2018-04-29 11:22:48 +1000

    Update ExtendedWireType to match spec

--- src/Hagar/WireProtocol/ExtendedWireType.cs
+++ src/Hagar/WireProtocol/ExtendedWireType.cs
@@ -2,9 +2,7 @@ namespace Hagar.WireProtocol
 {
     public enum ExtendedWireType : byte
     {
-        //Null = 0b00 << 3, // This tag signifies that a value is null.
-        //Unused = 0b01 << 3, // This tag is not used in this version of the protocol.
-        EndTagDelimited = 0b10 << 3, // This tag marks the end of a tag-delimited object.
-        EndBaseFields = 0b11 << 3, // This tag marks the end of a base object in a tag-delimited object.
+        EndTagDelimited = 0b00 << 3, // This tag marks the end of a tag-delimited object.
+        EndBaseFields = 0b01 << 3, // This tag marks the end of a base object in a tag-delimited object.
     }
 }
