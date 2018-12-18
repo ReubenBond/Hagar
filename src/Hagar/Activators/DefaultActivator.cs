b6bde2bc83218da36d7e28091edd59bdb08ccd6e
Reuben Bond <reuben.bond@gmail.com>
2018-12-17 20:39:07 -0800

    Add CodecNotFoundException, clean up some ToDos (#38)

--- src/Hagar/Activators/DefaultActivator.cs
+++ src/Hagar/Activators/DefaultActivator.cs
@@ -4,7 +4,6 @@ namespace Hagar.Activators
     {
         public T Create()
         {
-            // TODO: consider array support using a different abstraction (need parameters)?
             return System.Activator.CreateInstance<T>();
         }
     }
