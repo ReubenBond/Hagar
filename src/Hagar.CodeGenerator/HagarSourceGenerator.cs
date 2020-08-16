fd8c7fd8f154ad69d309dbb4deaeb356b3c1e790
Reuben Bond <203839+ReubenBond@users.noreply.github.com>
2020-08-16 10:49:25 -0700

    Initial Source Generator implementation (#68)

---
+++ src/Hagar.CodeGenerator/HagarSourceGenerator.cs
@@ -0,0 +1,23 @@
+using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.Text;
+using System.Text;
+
+namespace Hagar.CodeGenerator
+{
+    [Generator]
+    public class HagarSourceGenerator : ISourceGenerator
+    {
+        public void Execute(SourceGeneratorContext context)
+        {
+            var codeGenerator = new CodeGenerator(context.Compilation, new CodeGeneratorOptions() );
+            var syntax = codeGenerator.GenerateCode(context.CancellationToken);
+            var sourceString = syntax.NormalizeWhitespace().ToFullString();
+            var sourceText = SourceText.From(sourceString, Encoding.UTF8);
+            context.AddSource("Hagar.g.cs", sourceText);
+        }
+
+        public void Initialize(InitializationContext context)
+        {
+        }
+    }
+}
