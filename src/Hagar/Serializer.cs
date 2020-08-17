35133c61c284589e130a2c17798827d74d634fe9
Reuben Bond <rebond@microsoft.com>
2020-08-16 21:13:11 -0700

    Scaffold user-friendly API

--- src/Hagar/Serializer.cs
+++ src/Hagar/Serializer.cs
@@ -2,12 +2,64 @@ using Hagar.Buffers;
 using Hagar.Codecs;
 using Hagar.GeneratedCodeHelpers;
 using Hagar.Serializers;
+using Hagar.Session;
 using Hagar.WireProtocol;
 using System;
 using System.Buffers;
+using System.IO;
 
 namespace Hagar
 {
+    public sealed class Serializer 
+    {
+        private readonly SessionPool _sessionPool;
+        private readonly ICodecProvider _codecProvider;
+
+        public byte[] SerializeToByteArray<T>(T value)
+        {
+            return default;
+        }
+
+        public T Deserialize<T>(byte[] source) => Deserialize<T>(new ReadOnlyMemory<byte>(source));
+        
+        public T Deserialize<T>(ReadOnlyMemory<byte> source)
+        {
+            var sequence = new ReadOnlySequence<byte>(source);
+            using var session = _sessionPool.GetSession();
+            var reader = new Reader(sequence, session);
+            var codec = _codecProvider.GetCodec<T>();
+            var field = reader.ReadFieldHeader();
+            return codec.ReadValue(ref reader, field);
+        }
+
+        public void Serialize<T>(T value, Stream destination)
+        {
+
+        }
+
+        public T Deserialize<T>(Stream source)
+        {
+            return default;
+        }
+
+        public void Serialize<T>(T value, IBufferWriter<byte> destination)
+        {
+            using var session = _sessionPool.GetSession();
+            var writer = new Writer<IBufferWriter<byte>>(destination, session);
+            var codec = _codecProvider.GetCodec<T>();
+            codec.WriteField(ref writer, 0, typeof(T), value);
+        }
+
+        public T Deserialize<T>(ReadOnlySequence<byte> source)
+        {
+            using var session = _sessionPool.GetSession();
+            var reader = new Reader(source, session);
+            var codec = _codecProvider.GetCodec<T>();
+            var field = reader.ReadFieldHeader();
+            return codec.ReadValue(ref reader, field);
+        }
+    }
+
     public sealed class Serializer<T>
     {
         private readonly IFieldCodec<T> _codec;
