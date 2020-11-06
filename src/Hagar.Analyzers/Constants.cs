a8c7625338f681c6ccec57cd0b43fe306f2ad435
Reuben Bond <203839+ReubenBond@users.noreply.github.com>
2020-11-06 07:22:00 -0800

    Analyzer improvements (#86)

---
+++ src/Hagar.Analyzers/Constants.cs
@@ -0,0 +1,13 @@
+﻿namespace Hagar.Analyzers
+{
+    internal static class Constants
+    {
+        public const string IdAttributeName = "Id";
+        public const string IdAttributeFullyQualifiedName = "global::Hagar.IdAttribute";
+        public const string GenerateSerializerAttributeName = "GenerateSerializer";
+        public const string SerializableAttributeName = "Serializable";
+        public const string NonSerializedAttribute = "NonSerialized";
+        public const string NonSerializedAttributeFullyQualifiedName = "global::System.NonSerializedAttribute";
+        public const string SystemNamespace = "System";
+    }
+}
