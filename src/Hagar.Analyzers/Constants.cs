4eb092618fefb92adab320f29d5654eb7b3abab8
Reuben Bond <203839+ReubenBond@users.noreply.github.com>
2020-11-18 13:36:56 -0800

    Analyzer/fix to add [GenerateSerializer] to [Serializable] types (#90)

--- src/Hagar.Analyzers/Constants.cs
+++ src/Hagar.Analyzers/Constants.cs
@@ -5,6 +5,7 @@
         public const string IdAttributeName = "Id";
         public const string IdAttributeFullyQualifiedName = "global::Hagar.IdAttribute";
         public const string GenerateSerializerAttributeName = "GenerateSerializer";
+        public const string GenerateSerializerAttributeFullyQualifiedName = "global::Hagar.GenerateSerializerAttribute";
         public const string SerializableAttributeName = "Serializable";
         public const string NonSerializedAttribute = "NonSerialized";
         public const string NonSerializedAttributeFullyQualifiedName = "global::System.NonSerializedAttribute";
