59901d9d7795f38361da3bf045428390f8a71bdb
Max Morrison <maxmorrison@microsoft.com>
2021-03-26 13:45:44 -0700

    Remove check that [Serializable] types have Hagar attributes

--- src/Hagar.Analyzers/GenerateHagarSerializationAttributesAnalyzer.cs
+++ src/Hagar.Analyzers/GenerateHagarSerializationAttributesAnalyzer.cs
@@ -31,7 +31,7 @@ namespace Hagar.Analyzers
         {
             if (context.Node is TypeDeclarationSyntax declaration && !declaration.Modifiers.Any(m => m.Kind() == SyntaxKind.StaticKeyword))
             {
-                if (declaration.HasAttribute(Constants.GenerateSerializerAttributeName) || declaration.HasAttribute(Constants.SerializableAttributeName))
+                if (declaration.HasAttribute(Constants.GenerateSerializerAttributeName)) // || declaration.HasAttribute(Constants.SerializableAttributeName))
                 {
                     var (serializableMembers, nextId) = SerializationAttributesHelper.AnalyzeTypeDeclaration(declaration);
                     if (serializableMembers.Count > 0)
@@ -43,35 +43,35 @@ namespace Hagar.Analyzers
         }
     }
 
-    [DiagnosticAnalyzer(LanguageNames.CSharp)]
-    public class AddGenerateHagarSerializationAttributeAnalyzer : DiagnosticAnalyzer
-    {
-        public const string RuleId = "HAGAR0002";
-        private const string Category = "Usage";
-        private static readonly LocalizableString Title = new LocalizableResourceString(nameof(Resources.AddSerializationAttributesTitle), Resources.ResourceManager, typeof(Resources));
-        private static readonly LocalizableString MessageFormat = new LocalizableResourceString(nameof(Resources.AddSerializationAttributesMessageFormat), Resources.ResourceManager, typeof(Resources));
-        private static readonly LocalizableString Description = new LocalizableResourceString(nameof(Resources.AddSerializationAttributesDescription), Resources.ResourceManager, typeof(Resources));
+    //[DiagnosticAnalyzer(LanguageNames.CSharp)]
+    //public class AddGenerateHagarSerializationAttributeAnalyzer : DiagnosticAnalyzer
+    //{
+    //    public const string RuleId = "HAGAR0002";
+    //    private const string Category = "Usage";
+    //    private static readonly LocalizableString Title = new LocalizableResourceString(nameof(Resources.AddSerializationAttributesTitle), Resources.ResourceManager, typeof(Resources));
+    //    private static readonly LocalizableString MessageFormat = new LocalizableResourceString(nameof(Resources.AddSerializationAttributesMessageFormat), Resources.ResourceManager, typeof(Resources));
+    //    private static readonly LocalizableString Description = new LocalizableResourceString(nameof(Resources.AddSerializationAttributesDescription), Resources.ResourceManager, typeof(Resources));
 
-        internal static DiagnosticDescriptor Rule { get; } = new(RuleId, Title, MessageFormat, Category, DiagnosticSeverity.Info, isEnabledByDefault: true, description: Description);
+    //    internal static DiagnosticDescriptor Rule { get; } = new(RuleId, Title, MessageFormat, Category, DiagnosticSeverity.Info, isEnabledByDefault: true, description: Description);
 
-        public override ImmutableArray<DiagnosticDescriptor> SupportedDiagnostics => ImmutableArray.Create(Rule);
+    //    public override ImmutableArray<DiagnosticDescriptor> SupportedDiagnostics => ImmutableArray.Create(Rule);
 
-        public override void Initialize(AnalysisContext context)
-        {
-            context.EnableConcurrentExecution();
-            context.ConfigureGeneratedCodeAnalysis(GeneratedCodeAnalysisFlags.Analyze);
-            context.RegisterSyntaxNodeAction(CheckSyntaxNode, SyntaxKind.ClassDeclaration, SyntaxKind.StructDeclaration);
-        }
+    //    public override void Initialize(AnalysisContext context)
+    //    {
+    //        context.EnableConcurrentExecution();
+    //        context.ConfigureGeneratedCodeAnalysis(GeneratedCodeAnalysisFlags.Analyze);
+    //        context.RegisterSyntaxNodeAction(CheckSyntaxNode, SyntaxKind.ClassDeclaration, SyntaxKind.StructDeclaration);
+    //    }
 
-        private void CheckSyntaxNode(SyntaxNodeAnalysisContext context)
-        {
-            if (context.Node is TypeDeclarationSyntax declaration && !declaration.Modifiers.Any(m => m.Kind() == SyntaxKind.StaticKeyword))
-            {
-                if (declaration.HasAttribute(Constants.SerializableAttributeName) && !declaration.HasAttribute(Constants.GenerateSerializerAttributeName)) 
-                {
-                    context.ReportDiagnostic(Diagnostic.Create(Rule, context.Node.GetLocation()));
-                }
-            }
-        }
-    }
+    //    private void CheckSyntaxNode(SyntaxNodeAnalysisContext context)
+    //    {
+    //        if (context.Node is TypeDeclarationSyntax declaration && !declaration.Modifiers.Any(m => m.Kind() == SyntaxKind.StaticKeyword))
+    //        {
+    //            if (declaration.HasAttribute(Constants.SerializableAttributeName) && !declaration.HasAttribute(Constants.GenerateSerializerAttributeName)) 
+    //            {
+    //                context.ReportDiagnostic(Diagnostic.Create(Rule, context.Node.GetLocation()));
+    //            }
+    //        }
+    //    }
+    //}
 }
